0781ee23a15c9fcf0a053902aa0811ba7ebc1d39
Finny Abraham <kozakk@gmail.com>
2019-11-18 16:03:20 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,4 +1,4 @@
-var playlist2 = {bieber:"believe"}
+var playlist = {bieber:"believe"}
 function updatePlaylist(playlist, artistName, songTitle) {
   Object.assign ( playlist, { [artistName] : songTitle } )
 }
